69fc59dcf8232cabf46ae508ccb4996fa220d1a1
Florian Roesler <florian.roesler@googlemail.com>
2019-07-01 22:16:06 +0200

    Split examples

--- index.js
+++ index.js
@@ -4,8 +4,18 @@ var path = require('path')
 var app = express()
 var port = 8080
 
+app.use(express.static('public'))
+
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname+'/index.html'))
+  res.sendFile(path.join(__dirname + '/index.html'))
+})
+
+app.get('/jspdf', function (req, res) {
+  res.sendFile(path.join(__dirname + '/views/jspdf.html'))
+})
+
+app.get('/html2pdf', function (req, res) {
+  res.sendFile(path.join(__dirname + '/views/html2pdf.html'))
 })
 
 app.listen(port, function () {
